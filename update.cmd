bd44a438a837e5ddb7f217fa594659417d9e2fbc
Denis Kharchenko <best-tech@users.noreply.github.com>
2017-09-30 15:56:14 +0300

    Update update.cmd

--- update.cmd
+++ update.cmd
@@ -7,21 +7,9 @@ cls
 @choco upgrade python -y
 @choco upgrade ruby -y
 @choco upgrade wget -y
-@choco upgrade pickles -y
-@choco upgrade picklesui -y
 @choco upgrade pandoc -y
 @choco upgrade irfanview -y
 echo Waiting for MSI for oscript.io
 echo choco upgrade oscript
 @choco upgrade sublimetext3 -y
-@choco upgrade atom -y
-@choco upgrade plantuml -y
-@choco upgrade chocolateygui -y
-
-echo addition cloud provider
-
-@choco upgrade virtualbox -y
-@choco upgrade vagrant -y
-@choco upgrade docker-machine -y
-@choco upgrade docker -y
-@choco upgrade docker-compose -y
+@choco upgrade chocolateygui  -y
